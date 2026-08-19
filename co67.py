8ddee12b0d2e4ee79f2aa5f3cfacd840f2eb9001
Mhfoud <jean.trio22@gmail.com>
2026-08-19 03:49:44 +0100

    Ajout du menu principal

--- co67.py
+++ co67.py
@@ -10,12 +10,30 @@ codes = {
     "P0420": "Efficacité du système catalyseur insuffisante."
 }
 
-code = input("Entrez un code défaut OBD-II : ").upper()
+while True:
+    print()
+    print("1. Lire un code défaut")
+    print("2. Quitter")
+    print()
 
-if code in codes:
-    print()
-    print("Code :", code)
-    print("Description :", codes[code])
-else:
-    print()
-    print("Code inconnu dans la base CO-67.")
+    choix = input("Votre choix : ")
+
+    if choix == "1":
+        code = input("Entrez un code défaut OBD-II : ").upper()
+
+        if code in codes:
+            print()
+            print("Code :", code)
+            print("Description :", codes[code])
+        else:
+            print()
+            print("Code inconnu dans la base CO-67.")
+
+    elif choix == "2":
+        print()
+        print("Merci d'avoir utilisé CO-67.")
+        break
+
+    else:
+        print()
+        print("Choix invalide. Entrez 1 ou 2.")
